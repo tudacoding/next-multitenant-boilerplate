aeaaae41580faa7f3abae941011315185c7a360b
Thinh Nguyen <thinhnguyen.teamx@gmail.com>
2025-04-17 17:34:43 +0700

    fix bug signin

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,15 @@
 import { LoginForm } from "@/components/login-form"
 
 export default function Page() {
+  const handleSubmit = async () => {
+    // Xử lý đăng nhập ở đây
+    console.log('Dữ liệu đăng nhập:')
+  }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <LoginForm onSubmit={handleSubmit} />
       </div>
     </div>
   )
